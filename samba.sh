d3c2aa9d55a9dc506bed4a467831e41e1fd75744
Kroese <kroese@users.noreply.github.com>
2024-02-09 00:41:09 +0100

    Update samba.sh

--- samba.sh
+++ samba.sh
@@ -4,6 +4,7 @@ set -Eeuo pipefail
 SHARE="/storage"
 
 mkdir -p "$SHARE"
-chmod -R 777 "$SHARE"
+chmod -R 0770 "$SHARE"
+chown root:smbgrp "$SHARE"
 
 smbd --foreground --log-stdout --no-process-group
