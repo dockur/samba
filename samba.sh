8780bdc5ceb388b54df9265901ccf29c110504db
Kroese <kroese@users.noreply.github.com>
2024-02-09 01:25:57 +0100

    Update samba.sh

--- samba.sh
+++ samba.sh
@@ -2,11 +2,11 @@
 set -Eeuo pipefail
 
 group="smb"
-
-adduser -S -D -H -h /tmp -s /sbin/nologin -G "$group" -g 'Samba User' "$USER" && \
-echo -e "$PASS\n$PASS" | smbpasswd -a -s "$USER"
-
 share="/storage"
+
+id -u "$USER" &>/dev/null || adduser -S -D -H -h /tmp -s /sbin/nologin -G "$group" -g 'Samba User' "$USER" > /dev/null
+echo -e "$PASS\n$PASS" | smbpasswd -a -s "$USER" > /dev/null
+
 mkdir -p "$share"
 chmod -R 0770 "$share"
 chown "$USER:$group" "$share"
