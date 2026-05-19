4f1f37eda254e21a0596f2b016c6f80297ecebd2
Kroese <kroese@users.noreply.github.com>
2026-05-19 05:39:05 +0200

    fear: Dynamicly adjust config file

--- samba.sh
+++ samba.sh
@@ -68,9 +68,6 @@ add_user() {
         echo -e "$password\n$password" | smbpasswd -a -c "$cfg" -s "$username" > /dev/null || { echo "Failed to add Samba user $username"; return 1; }
         [[ "$username" != "$USER" ]] && echo "User $username has been added to Samba and password set."
     fi
-
-    [ -z "$first_user" ] && first_user="$username"
-    [ -z "$first_group" ] && first_group="$groupname"
     
     return 0
 }
@@ -82,14 +79,6 @@ secret="/run/secrets/pass"
 config="/etc/samba/smb.conf"
 users="/etc/samba/users.conf"
 
-# Create shared directory
-mkdir -p "$share" || { echo "Failed to create directory $share"; exit 1; }
-
-# Create directories if missing
-mkdir -p /var/lib/samba/sysvol
-mkdir -p /var/lib/samba/private
-mkdir -p /var/lib/samba/bind-dns
-
 # Check if the secret file exists and if its size is greater than zero
 if [ -s "$secret" ]; then
     PASS=$(cat "$secret")
@@ -97,10 +86,25 @@ fi
 
 # Check if config file is not a directory
 if [ -d "$config" ]; then
-
     echo "The bind $config maps to a file that does not exist!"
     exit 1
+fi
 
+# Check if users file is not a directory
+if [ -d "$users" ]; then
+    echo "The bind $users maps to a file that does not exist!"
+    exit 1
+fi
+
+# Create shared directory
+mkdir -p "$share" || { echo "Failed to create directory $share"; exit 1; }
+
+# Set permissions for share directory if new (empty), leave untouched if otherwise
+if [ -z "$(ls -A "$share")" ]; then
+    chmod 0770 "$share" || echo "Failed to set permissions for directory $share"
+    if [ ! -s "$users" ]; then
+      chown "$USER:$group" "$share" || echo "Failed to set ownership for directory $share"
+    fi
 fi
 
 # Check if an external config file was supplied
@@ -115,8 +119,8 @@ else
     template="/etc/samba/smb.default"
 
     if [ ! -f "$template" ]; then
-      echo "Your /etc/samba directory does not contain a valid smb.conf file!"
-      exit 1
+        echo "Your /etc/samba directory does not contain a valid smb.conf file!"
+        exit 1
     fi
 
     # Generate a config file from template
@@ -125,7 +129,7 @@ else
 
     # Set custom display name if provided
     if [ -n "$NAME" ] && [[ "${NAME,,}" != "data" ]]; then
-      sed -i "s/\[Data\]/\[$NAME\]/" "$config"    
+        sed -i "s/\[Data\]/\[$NAME\]/" "$config"    
     fi
 
     # Verify if the RW variable is equal to false (indicating read-only mode) 
@@ -135,21 +139,21 @@ else
         sed -i "s/^\(\s*\)read only =.*/\1read only = yes/" "$config"
     fi
 
+    # Check if multi-user mode is enabled
+    if [ ! -s "$users" ] && [[ "$FORCE" == [Yy1]* ]]; then
+        # Add force user settings
+        echo "   force user = $USER" >> "$config"
+        echo "   force group = $group" >> "$config"
+    fi
+
 fi
-
-# Check if users file is not a directory
-if [ -d "$users" ]; then
-
-    echo "The file $users does not exist, please check that you mapped it to a valid path!"
-    exit 1
-
-fi
-
-first_user=""
-first_group=""
 
 # Check if multi-user mode is enabled
-if [ -s "$users" ]; then
+if [ ! -s "$users" ]; then
+
+    add_user "$config" "$USER" "$UID" "$group" "$GID" "$PASS" "$share" || { echo "Failed to add user $USER"; exit 1; }
+
+else
 
     while IFS= read -r line || [[ -n ${line} ]]; do
 
@@ -173,39 +177,15 @@ if [ -s "$users" ]; then
 
     done < <(tr -d '\r' < "$users")
 
-else
-
-    add_user "$config" "$USER" "$UID" "$group" "$GID" "$PASS" "$share" || { echo "Failed to add user $USER"; exit 1; }
-
-fi
-
-# Skip if a config file was supplied
-if [[ "$config" != "/etc/samba/smb.conf" && "$FORCE" == [Yy1]* ]]; then
-
-  # Check if multi-user mode
-  if [ ! -s "$users" ]; then
-
-    # Remove force settings from smb.conf
-    sed -i '/force user /d' "$config"
-    sed -i '/force group /d' "$config"
-
-  else
-
-    # Update force user and force group in smb.conf
-    sed -i "s/^\(\s*\)force user =.*/\1force user = $first_user/" "$config"
-    sed -i "s/^\(\s*\)force group =.*/\1force group = $first_group/" "$config"
-
-  fi
 fi
 
 # Store configuration location for Healthcheck
 ln -sf "$config" /etc/samba.conf
 
-# Set permissions for share directory if new (empty), leave untouched if otherwise
-if [ -z "$(ls -A "$share")" ]; then
-    chmod 0770 "$share" || echo "Failed to set permissions for directory $share"
-    chown "$first_user:$first_group" "$share" || echo "Failed to set ownership for directory $share"
-fi
+# Create directories if missing
+mkdir -p /var/lib/samba/sysvol
+mkdir -p /var/lib/samba/private
+mkdir -p /var/lib/samba/bind-dns
 
 # Set directory permissions
 [ -d /run/samba/msg.lock ] && chmod -R 0755 /run/samba/msg.lock || :
